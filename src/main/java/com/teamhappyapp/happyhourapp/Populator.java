91f15142f086c172c3b84f8300e201e50bbf4f72
Paige Ruppel <paige.ruppel@gmail.com>
2017-07-18 10:58:56 -0400

    added additional attributes to establishment and updated html template to show address and lat/lng

--- src/main/java/com/teamhappyapp/happyhourapp/Populator.java
+++ src/main/java/com/teamhappyapp/happyhourapp/Populator.java
@@ -13,13 +13,13 @@ public class Populator implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		Establishment bossygrrls = new Establishment("Bossy Grrl's");
+		Establishment bossygrrls = new Establishment("Bossy Grrl's PinUp Joint", "2598 N. High Street, Columbus, OH 43202", "40.0152724", "-83.0136126");
 		establishmentRepo.save(bossygrrls);
 		
-		Establishment aceofcups = new Establishment("Ace of Cups");
+		Establishment aceofcups = new Establishment("Ace of Cups", "2619 N High St, Columbus, OH 43202", "40.0152724", "-83.0136126");
 		establishmentRepo.save(aceofcups);
 		
-		Establishment tatoheads = new Establishment("Tatoheads");
+		Establishment tatoheads = new Establishment("Tatoheads Public House", "1297 Parsons Ave, Columbus, OH 43206", "39.9369271", "-82.9858721");
 		establishmentRepo.save(tatoheads);
 	}
 
