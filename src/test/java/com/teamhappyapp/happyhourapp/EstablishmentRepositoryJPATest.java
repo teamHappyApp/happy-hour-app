e12dd7b2e9faf0e34b22f1445500ad11408b1546
Paige Ruppel <paige.ruppel@gmail.com>
2017-07-20 15:17:04 -0400

    wrote jpa test for repo method to return schedule by start and end time

---
+++ src/test/java/com/teamhappyapp/happyhourapp/EstablishmentRepositoryJPATest.java
@@ -0,0 +1,34 @@
+package com.teamhappyapp.happyhourapp;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EstablishmentRepositoryJPATest {
+	
+	@Resource
+	private EstablishmentRepository establishmentRepo;
+	
+	private Schedule testSchedule;
+	
+	private Establishment testEstablishment;
+	
+	@Before
+	public void createTestEstablishment() {
+		testSchedule = new Schedule(5, 8);
+		testEstablishment = new Establishment("name", "address", "lat", "long", "phone", testSchedule);
+	}
+	
+	@Test
+	public void shouldReturnDemoInSet() {
+		establishmentRepo.save(testEstablishment);
+		establishmentRepo.findByScheduleStartTimeLessThanAndScheduleEndTimeGreaterThanEqual(6, 7);
+	}
+
+}
